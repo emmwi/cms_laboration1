d3a3535c0d32b64f2b4b43bf034748595bad9aa7
emmwi <emma.m.with@gmail.com>
2024-12-03 11:30:56 +0100

    lägger till rätt stavning på single.php

---
+++ wp-content/themes/EmmaWith_laboration1/page-kontakt.php
@@ -0,0 +1,20 @@
+page kontakt?? här hamnar kontakt
+<?php
+get_header();
+while (have_posts()) {
+  the_post(); ?>
+  <section>
+    <div class="container">
+      <div class="row">
+        <div id="primary" class="col-xs-12 col-md-9">
+          <h1> <?php the_title(); ?></h1>
+          <?php
+          the_content(); ?>
+        </div>
+
+      </div>
+    </div>
+  </section>
+<?php
+}
+get_footer();
